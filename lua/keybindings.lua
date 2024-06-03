ecefaf060ed55107eb151d0ae66612c378e16ddd
brianhuster <phambinhanctb2004@gmail.com>
2024-06-03 19:49:27 +0700

    update keybindings

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -1,14 +1,3 @@
--- Keybindings for Coc
-vim.api.nvim_set_keymap('n', 'gd', '<Plug>(coc-definition)', {silent = true})
-vim.api.nvim_set_keymap('n', 'gy', '<Plug>(coc-type-definition)', {silent = true})
-vim.api.nvim_set_keymap('n', 'gr', '<Plug>(coc-references)', {silent = true})
-vim.api.nvim_set_keymap('n', '[g', '<Plug>(coc-diagnostic-prev)', {silent = true})
-vim.api.nvim_set_keymap('n', ']g', '<Plug>(coc-diagnostic-next)', {silent = true})
-vim.api.nvim_set_keymap('n', '<space>s', ':<C-u>CocList -I symbols<cr>', {silent = true})
-vim.api.nvim_set_keymap('n', '<space>d', ':<C-u>CocList diagnostics<cr>', {silent = true})
-vim.api.nvim_set_keymap('n', '<leader>do', '<Plug>(coc-codeaction)', {})
-vim.api.nvim_set_keymap('n', '<leader>rn', '<Plug>(coc-rename)', {})
-
 function TerminalMode()
     local buf = vim.api.nvim_get_current_buf()
     if vim.fn.getbufvar(buf, '&buftype') == 'terminal' then
@@ -31,6 +20,22 @@ function TerminalMode()
     vim.cmd('startinsert')
 end
 
+function save_and_quit()
+    vim.cmd('wa')
+    vim.cmd('qa!')
+end
+
+-- Keybindings for Coc
+vim.api.nvim_set_keymap('n', 'gd', '<Plug>(coc-definition)', {silent = true})
+vim.api.nvim_set_keymap('n', 'gy', '<Plug>(coc-type-definition)', {silent = true})
+vim.api.nvim_set_keymap('n', 'gr', '<Plug>(coc-references)', {silent = true})
+vim.api.nvim_set_keymap('n', '[g', '<Plug>(coc-diagnostic-prev)', {silent = true})
+vim.api.nvim_set_keymap('n', ']g', '<Plug>(coc-diagnostic-next)', {silent = true})
+vim.api.nvim_set_keymap('n', '<space>s', ':<C-u>CocList -I symbols<cr>', {silent = true})
+vim.api.nvim_set_keymap('n', '<space>d', ':<C-u>CocList diagnostics<cr>', {silent = true})
+vim.api.nvim_set_keymap('n', '<leader>do', '<Plug>(coc-codeaction)', {})
+vim.api.nvim_set_keymap('n', '<leader>rn', '<Plug>(coc-rename)', {})
+
 -- Terminal keybindings
 vim.api.nvim_set_keymap('n', 't', ':lua TerminalMode()<CR>', {})
 vim.api.nvim_set_keymap('v', 't', ':lua TerminalMode()<CR>', {})
@@ -52,4 +57,4 @@ vim.api.nvim_set_keymap('i', '<C-p>', ':Prettier', {})
 vim.api.nvim_set_keymap('v', '<C-p>', ':PrettierFragment', {})
 
 --save and quit
-vim.api.nvim_set_keymap('n', ':wqa', ':wa<CR>:qa!<CR>', {})
+vim.api.nvim_set_keymap('n', 'sq', ':lua save_and_quit()<CR>', {})

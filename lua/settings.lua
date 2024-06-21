0cc84b652cd8fb4c672b8350ad58a336cad720a8
brianhuster <phambinhanctb2004@gmail.com>
2024-06-21 17:24:49 +0700

    update

--- lua/settings.lua
+++ lua/settings.lua
@@ -27,6 +27,55 @@ vim.cmd([[
     autocmd BufEnter * lua bufEnter()
 ]])
 
+
+-- Set up the use of ibus in neovim
+-- Define global variable to store the previous IBus engine
+_G.ibus_prev_engine = ""
+
+-- Function to turn off IBus (switch to English)
+function IBusOff()
+  -- Save the current engine
+  _G.ibus_prev_engine = vim.fn.system('ibus engine')
+  -- Switch to English engine
+  os.execute('ibus engine xkb:us::eng')
+end
+
+-- Function to turn on IBus (restore the previous engine)
+function IBusOn()
+  local current_engine = vim.fn.system('ibus engine')
+  -- If the engine was not set to English in normal mode,
+  -- save the current engine to restore it later
+  if not string.find(current_engine, 'xkb:us::eng') then
+    _G.ibus_prev_engine = current_engine
+  end
+  -- Restore the previous engine
+  os.execute('ibus engine ' .. _G.ibus_prev_engine)
+end
+
+vim.api.nvim_create_augroup("IBusHandler", { clear = true })
+vim.api.nvim_create_autocmd({"CmdLineEnter"}, {
+  pattern = "[/?]",
+  callback = IBusOn,
+  group = "IBusHandler",
+})
+vim.api.nvim_create_autocmd({"CmdLineLeave"}, {
+  pattern = "[/?]",
+  callback = IBusOff,
+  group = "IBusHandler",
+})
+vim.api.nvim_create_autocmd({"InsertEnter"}, {
+  pattern = "*",
+  callback = IBusOn,
+  group = "IBusHandler",
+})
+vim.api.nvim_create_autocmd({"InsertLeave"}, {
+  pattern = "*",
+  callback = IBusOff,
+  group = "IBusHandler",
+})
+
+IBusOff()
+
 -- General settings
 vim.o.mouse = 'a'
 vim.o.number = true

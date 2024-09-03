24bc6e14bf74ab83dfef2d4b07f0092e682f7614
brianhuster <phambinhanctb2004@gmail.com>
2024-09-03 13:56:27 +0700

    config lua_ls for neovim built-in LuaJIT

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,10 +1,37 @@
+local function nvim_builtin_lua_ls()
+    require 'lspconfig'.lua_ls.setup {
+        on_init = function(client)
+            local path = client.workspace_folders[1].name
+            if vim.loop.fs_stat(path .. '/.luarc.json') or vim.loop.fs_stat(path .. '/.luarc.jsonc') then
+                return
+            end
+
+            client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+                runtime = {
+                    -- Tell the language server which version of Lua you're using
+                    -- (most likely LuaJIT in the case of Neovim)
+                    version = 'LuaJIT'
+                },
+                -- Make the server aware of Neovim runtime files
+                workspace = {
+                    checkThirdParty = false,
+                    library = vim.api.nvim_get_runtime_file("", true)
+                }
+            })
+        end,
+        settings = {
+            Lua = {}
+        }
+    }
+end
+
 return {
     'williamboman/mason.nvim',
     dependencies = {
         'williamboman/mason-lspconfig.nvim',
         'neovim/nvim-lspconfig',
     },
-    config = function ()
+    config = function()
         local lang_servers = {
             "arduino_language_server",
             "bashls",
@@ -32,9 +59,14 @@ return {
         require("mason-lspconfig").setup {
             ensure_installed = lang_servers,
         }
-        for _, lsp in ipairs(lang_servers) do
-            require('lspconfig')[lsp].setup {}
-        end
-        require('lspconfig').ccls = {}
+        require("mason-lspconfig").setup_handlers {
+            function(server_name)
+                require("lspconfig")[server_name].setup {}
+            end,
+            ["lua_ls"] = function()
+                nvim_builtin_lua_ls()
+            end,
+        }
+        require('lspconfig').ccls.setup {}
     end,
 }

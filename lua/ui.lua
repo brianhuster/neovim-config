8bf50a1c98ab9624adafe02b0de3e71794433842
brianhuster <phambinhanctb2004@gmail.com>
2024-09-20 12:42:40 +0700

    update ui

--- lua/ui.lua
+++ lua/ui.lua
@@ -3,6 +3,11 @@ vim.opt.termguicolors = true
 vim.cmd(':colorscheme vim')
 vim.api.nvim_set_hl(0, 'NormalFloat', { bg = '#1e1e1e', fg = '#ffffff' })
 vim.api.nvim_set_hl(0, 'FloatBorder', { bg = '#1e1e1e', fg = '#ff79c6' })
+vim.api.nvim_set_hl(0, "Pmenu", { bg = "#1e1e1e", fg = "#c0caf5" })
+vim.api.nvim_set_hl(0, "PmenuSel", { bg = "#73daca", fg = "#1e1e2e", bold = true })
+vim.api.nvim_set_hl(0, "PmenuSbar", { bg = "#2e2e3e" })
+vim.api.nvim_set_hl(0, "PmenuThumb", { bg = "#bb9af7" })
+
 
 vim.api.nvim_create_autocmd('BufEnter', {
     pattern = '*',

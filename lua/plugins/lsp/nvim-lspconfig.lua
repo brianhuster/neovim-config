3e6da78c7f592c1ae04624ef5118ec59f28d43d4
brianhuster <phambinhanctb2004@gmail.com>
2024-10-05 19:01:06 +0700

    update

--- lua/plugins/lsp/nvim-lspconfig.lua
+++ lua/plugins/lsp/nvim-lspconfig.lua
@@ -5,17 +5,14 @@ return {
 			callback = function(args)
 				local client = vim.lsp.get_client_by_id(args.data.client_id)
 				if client.supports_method('textDocument/implementation') then
-					-- Create a keymap for vim.lsp.buf.implementation
 					vim.api.nvim_buf_set_keymap(args.buf, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>',
 						{ noremap = true, silent = true })
 				end
 				if client.supports_method('textDocument/completion') and vim.lsp.completion then
-					-- Enable auto-completion
 					vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
 					vim.o.completeopt = 'menuone,noinsert,fuzzy,noselect'
 				end
 				if client.supports_method('textDocument/formatting') then
-					-- Format the current buffer on save
 					vim.api.nvim_create_autocmd('BufWritePre', {
 						buffer = args.buf,
 						callback = function()

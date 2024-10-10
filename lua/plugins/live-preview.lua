61111dec08e86df4f06ae73d6974eec2d34f65df
brianhuster <phambinhanctb2004@gmail.com>
2024-10-10 16:34:01 +0700

    add instruction for native package

--- lua/plugins/live-preview.lua
+++ lua/plugins/live-preview.lua
@@ -2,5 +2,5 @@ return {
 	'brianhuster/live-preview.nvim',
 	branch = "dev",
 	ft = { 'html', 'markdown', 'asciidoc' },
-	config = function() require('live-preview').setup() end,
+	opts = {},
 }
--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,26 +1,30 @@
 return {
-    "stevearc/oil.nvim",
-    dependencies = {
-        "nvim-tree/nvim-web-devicons"
-    },
+	"stevearc/oil.nvim",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+		"refractalize/oil-git-status.nvim",
+	},
 
-    config = function()
-        require("oil").setup({
-            view_options = {
-                show_hidden = true,
-            },
-            float = {
-                -- Padding around the floating window
-                padding = 2,
-                max_width = 90,
-                win_options = {
-                    winblend = 0,
-                },
-            },
-        })
-        vim.keymap.set('n', '<M-e>', require('oil').toggle_float)
-        vim.keymap.set('i', '<M-e>', require('oil').toggle_float)
-        vim.keymap.set('t', '<M-e>', require('oil').toggle_float)
-        vim.keymap.set('x', '<M-e>', require('oil').toggle_float)
-    end,
+	config = function()
+		require("oil").setup({
+			view_options = {
+				show_hidden = true,
+			},
+			float = {
+				-- Padding around the floating window
+				padding = 2,
+				max_width = 90,
+				win_options = {
+					winblend = 0,
+				},
+			},
+			win_options = {
+				signcolumn = "yes:2",
+			},
+		})
+		vim.keymap.set('n', '<M-e>', require('oil').toggle_float)
+		vim.keymap.set('i', '<M-e>', require('oil').toggle_float)
+		vim.keymap.set('t', '<M-e>', require('oil').toggle_float)
+		vim.keymap.set('x', '<M-e>', require('oil').toggle_float)
+	end,
 }
